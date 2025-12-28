df24f5a74fb0147e3bd23f7edfe2aeac98c29d9c
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2025-12-28 04:54:13 -0300

    feat(auth): cria controllers e rotas de autenticação

--- routes/web.php
+++ routes/web.php
@@ -2,15 +2,48 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Validation\Rules\In;
-
+use App\Http\Controllers\Auth\LoginController;
+use Illuminate\Cache\Console\ForgetCommand;
 use Inertia\Inertia;
 
+/**
+ * Web Route
+ */
 Route::get('/', function () {
-    return view('welcome');
-});
-
-
-Route::get('/testes', function () {
     return Inertia::render('Testes');
 });
+
+/**
+ * Testes Route
+ */
+Route::get('/teste', function () {
+    return Inertia::render('Testes');
+});
+
+/**
+ * Authentication Routes
+ */
+Route::middleware(['guest'])->group(function () {
+    /**
+     * Login
+     */
+    Route::get('/entrar', [LoginController::class, 'index'])->name('auth.login');
+    Route::post('/entrar', [LoginController::class, 'store'])->name('auth.login.store');
+
+    /**
+     * Cadastro
+     */
+    Route::get('/cadastrar', [RegisterController::class, 'create'])->name('auth.register');
+    Route::post('/cadastar', [RegisterController::class, 'store'])->name('auth.register.store');
+
+    /**
+     * Recuperação de Senha | Esqueci a Senha
+     */
+    Route::get('/recuperar-senha', [ForgetPasswordController::class, 'update'])->name('auth.password');
+    Route::post('/recuperar-senha', [ForgetPasswordController::class, 'store'])->name('auth.password.email');
+});
+
+Route::get('/entrar', function () {
+    return Inertia::render('auth/Login');
+})->name('auth.login');
 
