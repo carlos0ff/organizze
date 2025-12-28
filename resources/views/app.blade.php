cf9d7c6208e64484faa3a391fdad0546e01133b4
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2025-12-28 04:54:30 -0300

    style(layout): ajusta footer e layout base

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,17 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-    <!-- Fonts -->
+    <!-- Google Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-    <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <!-- Core stylesheet -->
+    @vite('resources/css/app.css')
     @inertiaHead
+
 </head>
 <body class="font-sans antialiased">
-    @inertia
+
+<!-- Inertia.js root element -->
+@inertia
+
+<!-- Core JavaScript -->
+@vite('resources/js/app.js')
 </body>
 </html>
