172492fbc2597991b868ccc41f754c441ac5f566
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2026-01-02 17:49:33 -0300

    feat: adiciona botões de login social Google e Facebook

---
+++ resources/js/components/buttons/FacebookAuthButton.vue
@@ -0,0 +1,43 @@
+
+
+<template>
+  <button
+    type="button"
+    aria-label="Continuar com Facebook"
+    class="
+      group
+      flex w-full items-center justify-center gap-3
+      rounded-md
+      bg-[#1877F2]
+      px-6 py-3
+      text-sm sm:text-base
+      font-medium text-white
+      transition-all duration-300 ease-in-out
+      hover:bg-[#166FE5]
+      focus-visible:outline-none
+      focus-visible:ring-2 focus-visible:ring-[#1877F2] focus-visible:ring-offset-2
+      cursor-pointer
+      mb-10
+    "
+  >
+    <svg
+      width="12"
+      height="24"
+      viewBox="0 0 12 24"
+      xmlns="http://www.w3.org/2000/svg"
+      class="
+        shrink-0
+        fill-current
+        transition-colors duration-300
+        text-white
+      "
+      aria-hidden="true"
+    >
+      <path d="M7.78896 23.9738V13.4848H11.3269L11.8566 9.3971H7.78896V6.78721C7.78896 5.60374 8.11924 4.79715 9.82479 4.79715L12 4.79615V1.14015C11.6236 1.09051 10.3325 0.979248 8.83037 0.979248C5.69421 0.979248 3.54712 2.88415 3.54712 6.38258V9.39721H0V13.4849H3.54701V23.9739L7.78896 23.9738Z" />
+    </svg>
+
+    <span class="transition-colors duration-300">
+      Continuar com Facebook
+    </span>
+  </button>
+</template>
