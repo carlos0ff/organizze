5809e8cb3d6f863c4b09f7042b2416bd8743c419
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2026-01-02 17:49:27 -0300

    refactor: atualiza componentes de input e base da UI

---
+++ resources/js/components/inputs/EmailInput.vue
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<template>
+  <div class="flex flex-col relative mb-4 w-full">
+    <label for="email" class="text-[#4D4D4D] font-semibold mb-2">Seu email</label>
+    <div class="group relative flex flex-col items-center">
+      <input id="email" type="email"  placeholder="Digite seu email" required
+      class="border-1 border-[#DBDED8] bg-[#FEFDF9] h-12 w-full rounded-md px-4 transition-all duration-150 ease-in-out focus:border-[#1DB968] focus:shadow-focus-input-shadow focus:ring-0 group-focus-within:z-10border-1 border-[#DBDED8] bg-[#FEFDF9] h-12 w-full rounded-md px-4 transition-all duration-150 ease-in-out focus:border-[#1DB968] focus:shadow-focus-input-shadow focus:ring-0 group-focus-within:z-10" />
+
+    </div>
+  </div>
+</template>
