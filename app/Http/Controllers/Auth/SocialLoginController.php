84f49a78f3074e7c6788aaf3eeb7e17e3c1a30c7
José Carlos da Silva Júnior <carlosiilva66@gmail.com>
2026-02-13 23:27:50 -0300

    feat(auth): implement social login integration

---
+++ app/Http/Controllers/Auth/SocialLoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Laravel\Socialite\Facades\Socialite;
+
+class SocialLoginController extends Controller
+{
+    /**
+     * Redireciona o usuário para a página de autenticação do Google
+     */
+    public  function redirectToProvider()
+    {
+
+    }
+
+    /**
+     * Obtém as informações do usuário do Google e faz o login
+     */
+    public function handleProviderCallback() 
+    {
+
+    }
+}
